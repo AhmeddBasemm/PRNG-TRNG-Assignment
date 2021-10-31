c7966a54aea02a1fc945b83b104b604586171093
AhmeddBasemm <93495106+AhmeddBasemm@users.noreply.github.com>
2021-11-01 00:52:16 +0200

    Final

---
+++ Code/Install-Packages.bat
@@ -0,0 +1,10 @@
+echo Ahmed Basem
+echo This is a simple batch script to install numpy and Tkinter
+
+
+python --version
+pip -V
+pip install tk
+pip install numpy
+
+set /p DUMMY=Hit ENTER to continue...
